8740ca1add351b6c952d6dc129156ca769a85d1e
Pawel <you@example.com>
2022-11-16 19:32:35 +0100

    modele

--- charity_donations/charity_app/models.py
+++ charity_donations/charity_app/models.py
@@ -1,3 +1,37 @@
+"""Import base built in django model USER"""
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Institution(models.Model):
+    Institution_CHOICES = (
+        ('FDN', "Foundation"),
+        ('NGO', 'Non-governmental organization'),
+        ('L-CALL', 'Local collection')
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.CharField(max_length=10, choices=Institution_CHOICES,
+                            default='FDN')
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=100)
+    zip_code = models.IntegerField()
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
+                             default=True)
